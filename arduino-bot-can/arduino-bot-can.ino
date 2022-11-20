f2f856b08d10f7ca184de782e0e8d553487ee173
tan <lehoangtan1604@gmail.com>
2022-11-20 23:03:43 +0700

    thêm truyền thông với arduino

---
+++ arduino-bot-can/arduino-bot-can.ino
@@ -0,0 +1,85 @@
+#include <Servo.h>
+#define numOfValsRec 3
+#define digitsPerValRec 3
+int valsRec[numOfValsRec];
+int stringLength=numOfValsRec*digitsPerValRec+1;
+int counter =0;
+bool counterStart=false;
+String receivedString;
+
+String myS="123,456";
+int flag=1;
+
+
+
+
+Servo myservo_red;
+Servo myservo_blue;
+void setup() {
+valsRec[0]=000;
+valsRec[1]=111;
+valsRec[2]=222;
+Serial.begin(9600);
+pinMode(LED_BUILTIN, OUTPUT);
+
+myservo_red.attach(9); 
+myservo_blue.attach(10); 
+}
+
+void receiveData(){
+if(flag==0){
+ 
+  while (Serial.available()){
+    //Serial.println(__LINE__);
+    char c = Serial.read();
+    if (c=='$'){
+      counterStart=true;
+    }
+    if (counterStart){
+      if(counter<stringLength){
+        receivedString=String(receivedString+c);
+      counter++;
+      }
+      if(counter>=stringLength){
+       for (int i=0;i<numOfValsRec;i++){
+        int num=(i*digitsPerValRec)+1;
+        valsRec[i]=receivedString.substring(num,num+digitsPerValRec).toInt();
+       }
+       receivedString="";
+       counter=0;
+       counterStart=false;
+      }
+    }
+    flag=1;
+  }
+  Serial.flush();
+
+  }
+}
+
+
+void loop() {
+
+
+
+if(flag==1){
+myS="";
+//Serial.println(__LINE__);
+myS=String(valsRec[0]+','+valsRec[1]+','+valsRec[2]);
+Serial.print(valsRec[0]);
+Serial.print(",");
+Serial.print(valsRec[1]);
+Serial.print(",");
+Serial.println(valsRec[2]);
+Serial.flush();
+flag=0;
+
+  }
+
+receiveData();
+
+//-----------------------------do something -------------------------------
+
+
+//-----------------------------end do something -------------------------------
+}
--- jetsoninfer.py
+++ jetsoninfer.py
@@ -3,6 +3,12 @@ import jetson.utils
 import time
 import cv2
 import numpy as np
+import serial
+import time
+
+spilitdata=[0,0,0]
+flag=1
+
 width=640
 height=480
 cam=jetson.utils.gstCamera(width,height,'/dev/video0')
@@ -13,25 +19,53 @@ timeMark=time.time()
 font=cv2.FONT_HERSHEY_SIMPLEX
 startTime =  time.time()
 dtav = 0
+
+def initConnection(port,baud):
+    try:
+        ser=serial.Serial(port,baud)
+        print("Device connected")
+        return ser
+    except:
+        print("Errorrrrrrr")
+def sendData(se,data,digits):
+    global flag
+    myString="$"
+    for d in data:
+        myString+= str(d).zfill(digits)
+    try:
+        se.write(myString.encode())
+        flag=1
+        print(myString)
+    except:
+        print("send fail")
+
+ser=initConnection("/dev/ttyUSB0",9600)
 while True:
+    
+    if flag==0:
+        sendData(ser,[spilitdata[0],spilitdata[1],spilitdata[2]],3)
+    if flag==1:
+        while (ser.inWaiting() != 0):
+            data = ser.readline()
+            data = str(data, 'utf-8')
+            data = data.strip('\r\n')
+            spilitdata = data.split(",")
+            flag=0
+
+
     frame, width, height = cam.CaptureRGBA(zeroCopy=1)
     detections=net.Detect(frame, width, height)
-
-    print("detected {:d} objects in image".format(len(detections)))
-
     for detection in detections:
-        print(detection)
-
+        print("detected {:d} objects in image".format(detection.ClassID))
     frame=jetson.utils.cudaToNumpy(frame,width,height,4)
     frame=cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR).astype(np.uint8)
-
     cv2.putText(frame,str( round(net.GetNetworkFPS())),(0,30),font,1,(0,0,255),2)
-
     dt = time.time() - startTime
     startTime = time.time()
     dtav = 0.9 * dtav + 0.1 * dt
     fps = 1 / dtav
     cv2.putText(frame, str(round(fps, 1)) + ' fps', (450, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+
     cv2.imshow('webCam',frame)
     cv2.moveWindow('webCam',0,0)
     if cv2.waitKey(1)==ord('q'):
